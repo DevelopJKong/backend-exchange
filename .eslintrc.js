14a671e51bca47b2f1b93e53701f40912fadd057
커피볶는정콩 <61773021+DevelopJKong@users.noreply.github.com>
2022-10-05 11:55:12 +0900

    eslint & prettier 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,8 +22,6 @@ module.exports = {
     'prettier/prettier': {
       'no-empty-function': 'warn', // 중괄호가 빈 함수 허용
       '@typescript-eslint/no-empty-function': 'warn', // 중괄호가 빈 함수 허용
-      'react/prop-types': 'off', // prop-types 사용하지 않음
-      'react/react-in-jsx-scope': 'off', // React를 import하지 않아도 사용 가능
       camelcase: 'warn', // camelcase 사용하지 않음
       'spaced-comment': 'warn', // 주석 앞에 공백을 넣지 않음
       quotes: ['error', 'single', { allowTemplateLiterals: true }], // 따옴표 사용
